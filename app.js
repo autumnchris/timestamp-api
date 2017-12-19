c36c9876e624e22d47b63499d2de3e5013e1ed3b
Autumn <autumncbullard@gmail.com>
2017-12-19 18:20:50 -0500

    Add command that serves the HTML file to the homepage

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
 app.listen(port, console.log('Server is listening at port ' + port + '.'));
