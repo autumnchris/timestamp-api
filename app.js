42ae5c0b2ecd27320c2189dd3444f808ab39f68a
autumnchris <autumncbullard@gmail.com>
2020-01-13 01:11:54 -0500

    Update syntax

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use('/', indexRouter);
 app.use('/api/timestamp', apiRouter);
 
 app.use((req, res, next) => {
-    res.status(404).render('404.ejs', {title: 'Page not found | '});
+    res.status(404).render('404', {title: 'Page not found | '});
 });
 
 app.listen(port, console.log(`Server is listening at port ${port}.`));
